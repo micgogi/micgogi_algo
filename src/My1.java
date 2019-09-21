62ec62b1c1cb29e1b2393742fe06a502e3167351
micgogi <rahul.goggyani@gmail.com>
2019-09-22 01:37:12 +0530

    insertion added

--- src/My1.java
+++ src/My1.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class My1 {
 
     public static void main(String args[]){
-        System.out.println("Hello world");
+       int a[] = {5,58,7,8,9,2,1};
+       for(int i=1;i<a.length;i++){
+           int j=i-1;
+           int key = a[i];
+           while(j>=0&&a[j]>key){
+               a[j+1]=a[j];
+               j=j-1;
+           }
+           a[j+1]=key;
+
+       }
+
+        System.out.println(Arrays.toString(a));
     }
 }

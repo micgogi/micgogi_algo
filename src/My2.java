8e1ddca9412f7c0fb1a230c3e6f01484b7667fe3
micgogi <rahul.goggyani@gmail.com>
2019-09-23 18:25:43 +0530

    merge algo added

--- src/My2.java
+++ src/My2.java
@@ -1,2 +1,47 @@
-public class My2 {
+import java.util.Arrays;
+
+public class My2{
+    public static void main(String args[]){
+        int a[] = {2,5,5,8,5,4};
+        mergeSort(a,0,a.length-1);
+        System.out.println(Arrays.toString(a));
+        
+    }
+
+    public static void merge(int a[], int p , int q , int r) {
+    	int n1 = q-p+1;
+    	int n2 = r-q;
+    	int L[] = new int[n1+1];
+    	int R[] = new int[n2+1];
+    	for(int i=0;i<n1;i++) {
+    		L[i]= a[p+i];
+    	}
+    	for(int j=0;j<n2;j++) {
+    		R[j]= a[q+j+1];
+    	}
+    	L[n1]=Integer.MAX_VALUE;
+    	R[n2]=Integer.MAX_VALUE;
+    	int i=0,j=0;
+    	
+    	for(int k=p;k<=r;k++) {
+    		if(L[i]<=R[j]) {
+    			a[k]=L[i];
+    			i++;
+    		}else {
+    			a[k]=R[j];
+    			j++;
+    		}
+    	}
+    }
+    public static void mergeSort(int a[] , int p , int r) {
+    	int q=0;
+    	if(p<r) {
+    		q=(int) Math.floor((p+r)/2);
+    		System.out.println(q);
+        	mergeSort(a,p,q);
+    		mergeSort(a,q+1,r);
+    		merge(a,p,q,r);
+    	}
+    		
+    }
 }

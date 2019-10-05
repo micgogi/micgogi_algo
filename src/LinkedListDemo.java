0411b5d2dca90ef58600fa6f196c090e207621f5
micgogi <rahul.gogyani@gmail.com>
2019-10-05 11:45:05 +0530

    LinkedlISt

--- src/LinkedListDemo.java
+++ src/LinkedListDemo.java
@@ -8,11 +8,36 @@ public class LinkedListDemo {
             this.next = null;
         }
     }
+    public static void printAfterNode(int data, int k){
+        Node n = new Node(data);
+        Node temp = head;
+        while(temp.data!=k){
+            temp = temp.next;
+        }
+        Node temp1 = temp.next;
+        temp.next=n;
+        n.next=temp1;
+    }
     public static void printList(Node temp){
         while(temp!=null){
             System.out.println(temp.data);
             temp = temp.next;
         }
+    }
+    public static void printAtTheEnd(int data){
+        Node n = new Node(data);
+        Node temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next=n;
+    }
+    public static void insertAtStart(int data){
+        Node n = new Node(data);
+        Node temp = head;
+        head=n;
+        head.next =temp;
+
     }
     public static void main(String args[]){
         head = new Node(1);
@@ -22,7 +47,12 @@ public class LinkedListDemo {
         head.next = two;
         two.next = three;
         three.next = four;
+       // printList(head);
+       // insertAtStart(0);
+      //  printAtTheEnd(9);
+        printAfterNode(7,3);
         printList(head);
+
 
     }
 }

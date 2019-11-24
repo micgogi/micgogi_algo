a81dd0e070fd30837b631e4cc306c9d4e2d59b04
micgogi <rahul.gogyani@gmail.com>
2019-11-24 22:16:18 +0530

    CC_ALPHABET

---
+++ src/ALPHABET.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * @author Micgogi
+ * on 11/24/2019  9:55 PM
+ * 65 78 75 73 84 65
+ */
+public class ALPHABET {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String s = br.readLine();
+            char[] c = s.toCharArray();
+            int n = Integer.parseInt(br.readLine());
+            boolean flag =false;
+            for (int i = 0; i <n ; i++) {
+                String s1 = br.readLine();
+                char c1[] = s1.toCharArray();
+                Arrays.sort(c1);
+                Arrays.sort(c);
+
+                for (int j = 0; j <c.length ; j++) {
+                        if(c[j]==c1[j]){
+                            flag=true;
+                        }else{
+                            flag=false;
+                            break;
+                        }
+                }
+
+                if(flag) System.out.println("Yes");
+                else System.out.println("No");
+
+
+            }
+        }catch (Exception e){
+
+        }
+    }
+}

ea857ddff3dd21ed85023cd242b2a55d358a177a
micgogi <rahul.gogyani@gmail.com>
2019-12-14 19:11:02 +0530

    Create LC94.java

---
+++ src/LC94.java
@@ -0,0 +1,17 @@
+public class LC94 {
+    List<Integer> l = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return l;
+        
+    }
+    
+     public  void inorder(TreeNode temp){
+        if(temp!=null){
+            inorder(temp.left);
+           // System.out.println(temp.data);
+            l.add(temp.val);
+            inorder(temp.right);
+        }
+    }
+}

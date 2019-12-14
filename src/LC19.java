dc50321c19cb14c196f3eed73af9ac1b00eb5739
micgogi <rahul.gogyani@gmail.com>
2019-12-14 22:40:07 +0530

    Changes

--- src/LC19.java
+++ src/LC19.java
@@ -26,16 +26,36 @@ public class LC19 {
         prev.next = temp.next;
         return null;
     }
+    public static Node removeNthElementFromEnd(Node temp, int n){
+        Node dummy = new Node(0);
+        dummy.next = temp;
+        Node f = dummy;
+        Node s = dummy;
+        for (int i = 0; i <=n; i++) {
+            f = f.next;
+        }
+        while (f!=null){
+            f=f.next;
+            s=s.next;
+        }
+        s.next = s.next.next;
+        return dummy.next;
+    }
     public static void main(String[] args) {
         root = new Node(1);
-        root.next = new Node(2);
-        root.next.next = new Node(3);
-        root.next.next.next = new Node(4);
-        root.next.next.next.next = new Node(5);
-        removeNthElement(root,2);
-        while (root!=null){
-            System.out.println(root.data);
-            root = root.next;
+//        root.next = new Node(2);
+//        root.next.next = new Node(3);
+//        root.next.next.next = new Node(4);
+//        root.next.next.next.next = new Node(5);
+//        removeNthElement(root,2);
+//        while (root!=null){
+//            System.out.println(root.data);
+//            root = root.next;
+//        }
+        Node temp = removeNthElementFromEnd(root,1);
+        while (temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
         }
     }
 }

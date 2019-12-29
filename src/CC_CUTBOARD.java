80e226f0f62dbce3c0d16c5edb810ab52b7411e8
micgogi <rahul.gogyani@gmail.com>
2019-12-29 19:52:19 +0530

    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

---
+++ src/CC_CUTBOARD.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Micgogi
+ * on 12/29/2019  7:42 PM
+ * Micgogi
+ */
+public class CC_CUTBOARD {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            int t = Integer.parseInt(br.readLine());
+            while (t--!=0){
+                String s = br.readLine();
+                String s1[] = s.split(" ");
+                int n = Integer.parseInt(s1[0]);
+                int m = Integer.parseInt(s1[1]);
+                System.out.println((n-1)*(m-1));
+            }
+        }catch (Exception e){
+
+        }
+    }
+}

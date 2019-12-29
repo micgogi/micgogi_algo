e1ffeb14e5488e2e0625f2e16f8d007c23e80047
micgogi <rahul.gogyani@gmail.com>
2019-12-29 19:31:15 +0530

    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

---
+++ src/CC_DECINC.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Micgogi
+ * on 12/29/2019  7:27 PM
+ * Micgogi
+ */
+public class CC_DECINC {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int n = Integer.parseInt(br.readLine());
+            if(n%4==0){
+                System.out.println(++n);
+            }else {
+                System.out.println(--n);
+            }
+        }catch (Exception e){
+
+        }
+    }
+}

7333593f3faca38a2ef189a3afd293e9e79dd1ad
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-02-19 14:30:08 +0530

    Create LC75.java

---
+++ src/LC75.java
@@ -0,0 +1,31 @@
+/*
+ *@author Rahul Gogyani
+ *
+ * 1:55 PM 2/19/2020
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LC75 {
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int a[] = {2,0,2,1,1,0};
+        for (int i = 0; i <a.length ; i++) {
+            if(!map.containsKey(a[i])){
+                map.put(a[i],1);
+            }else{
+                map.put(a[i],map.get(a[i])+1);
+            }
+        }
+        int j=0;
+        for (int i:map.keySet()){
+            System.out.println(i+" "+map.get(i));
+            for (int k = 0; k <map.get(i) ; k++) {
+                a[j]=i;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}

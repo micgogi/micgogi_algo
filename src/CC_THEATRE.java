5974c93ff3ad347f51a345c97bceef30918ed486
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-02-21 14:36:42 +0530

    Update and rename CC_CASH.java to CC_THEATRE.java

--- src/CC_THEATRE.java
+++ src/CC_THEATRE.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class CC_CASH {
+public class CC_THEATRE {
     public static void main(String[] args) {
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

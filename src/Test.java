ce92d2bdb2a228fff929ca66fb8290699fbcf762
micgogi <rahul.gogyani@gmail.com>
2020-03-09 20:16:16 +0530

    LC160

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Micgogi
  * on 11/18/2019  2:17 AM
  * 65 78 75 73 84 65
  */
 public class Test {
-    public void main(String[] args) {
 
-    }
+
 }

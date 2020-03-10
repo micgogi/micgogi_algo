43100f9b8b5a867d95fbd36b57dc60b84849c6e2
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-03-10 15:25:34 +0530

    Create LC125.java

---
+++ src/LC125.java
@@ -0,0 +1,35 @@
+/*
+ *@author Rahul Gogyani
+
+ *
+ * 2:58 PM 3/10/2020
+ */
+
+import java.util.Stack;
+
+public class LC125 {
+
+    public static void main(String[] args) {
+       String s =".,";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)) {
+                stack.push(c);
+            }
+        }
+        boolean flag = true;
+        for (char c: s.toCharArray()){
+            if(Character.isLetterOrDigit(c)) {
+            if(stack.pop().toString().equalsIgnoreCase(""+c)){
+                flag = true;
+            }else{
+                flag = false;
+                break;
+            }}
+
+        }
+        System.out.println(flag);
+
+    }
+}

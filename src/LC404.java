d95f29667610c2ad0a4419a684fd405bf7096413
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-03-12 10:21:05 +0530

    Create LC404.java

---
+++ src/LC404.java
@@ -0,0 +1,36 @@
+/*
+ *@author Rahul Gogyani
+
+ *
+ * 10:08 AM 3/12/2020
+ */
+
+public class LC404 {
+    static TreeNode root;
+    static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        sum(root,false);
+        System.out.println(sum);
+    }
+    static int sum=0;
+    public static void sum(TreeNode root, boolean check){
+        if(root==null)return;
+        if(root.left==null&&root.right==null&&check){
+            sum+=root.data;
+        }
+        sum(root.left,true);
+        sum(root.right,false);
+  }
+}

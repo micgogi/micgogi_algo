c791d705faa0b8e66fb25e3e29aff397dd6bc91b
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-03-12 13:10:09 +0530

    Create LC104.java

---
+++ src/LC104.java
@@ -0,0 +1,28 @@
+public class LC104 {
+
+    static TreeNode root;
+    static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.println(maxDepth(root));
+
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}

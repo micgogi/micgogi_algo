0c2d6a434023a7bf3eefed8ff29850615d26bcdd
micgogi <rahul.gogyani@gmail.com>
2020-04-07 10:27:49 +0530

    LC219

---
+++ src/LC219.java
@@ -0,0 +1,32 @@
+import sun.awt.image.IntegerInterleavedRaster;
+
+import java.util.HashMap;
+
+/**
+ * @author Micgogi
+ * on 4/7/2020  10:16 AM
+ * Rahul Gogyani
+ */
+public class LC219 {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,1,2,3};
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(a,k));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i <nums.length ; i++) {
+            if(hashMap.containsKey(nums[i])){
+                int j = hashMap.get(nums[i]);
+                int diff = Math.abs(i-j);
+                if(diff<=k){
+                    return true;
+                }
+            }
+            hashMap.put(nums[i],i);
+        }
+
+        return false;
+    }
+}

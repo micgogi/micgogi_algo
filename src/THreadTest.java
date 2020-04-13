dd8373de018653ea37ea92b47e6e2fce5428c5a5
micgogi <rahul.gogyani@gmail.com>
2020-04-14 00:20:03 +0530

    657875738465

--- src/THreadTest.java
+++ src/THreadTest.java
@@ -1,6 +1,6 @@
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
+import java.util.concurrent.*;
 
 /**
  * @author Micgogi
@@ -11,12 +11,27 @@ public class THreadTest {
     public static void main(String[] args) {
         int coreCounts = Runtime.getRuntime().availableProcessors();
         System.out.println(coreCounts);
+        //Fixed Thread pools
         ExecutorService executorService = Executors.newFixedThreadPool(coreCounts);
+        //CachedThreadPool
+        ExecutorService executorService1 = Executors.newCachedThreadPool();
+        //ScheduledThreadPool
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        //task to run after 10 seconds delay
+        scheduledExecutorService.schedule(new Task(),10, TimeUnit.SECONDS);
+        //task to run repeatedly every 10 seconds
+        scheduledExecutorService.scheduleAtFixedRate(new Task(),15,10,TimeUnit.SECONDS);
+        //task to run repeatedly 10 seconds after previous task completes
+        scheduledExecutorService.scheduleWithFixedDelay(new Task(),15,10,TimeUnit.SECONDS);
+
+        //SingleThreadedExceutor
+
 
         for (int i = 0; i < 100; i++) {
-            executorService.execute(new Task());
+         //   executorService.execute(new Task());
+            executorService1.execute(new Task());
         }
-
+        executorService.shutdown();
     }
 }
 

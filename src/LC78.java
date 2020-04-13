98ed80a51485e0a6a37f75d13e1c5b5ef4648dc0
micgogi <rahul.gogyani@gmail.com>
2020-04-13 19:11:37 +0530

    LC78

---
+++ src/LC78.java
@@ -0,0 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Micgogi
+ * on 4/13/2020  5:25 PM
+ * Rahul Gogyani
+ */
+public class LC78 {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3};
+        System.out.println(subsets(nums));
+    }
+    public static List<List<Integer>> subsets(int nums[]){
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+    private static void  backtrack(List<List<Integer>> list, List<Integer> tempList, int nums[], int start){
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i <nums.length; i++) {
+            tempList.add(nums[i]);
+            backtrack(list,tempList, nums, i+1);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}

8e2b7c5363503e97f271634699ea7e7d42211dd0
micgogi <rahul.gogyani@gmail.com>
2020-04-16 14:55:22 +0530

    LC678

---
+++ src/LC678.java
@@ -0,0 +1,34 @@
+/**
+ * @author Micgogi
+ * on 4/16/2020  2:24 PM
+ * Rahul Gogyani
+ */
+public class LC678 {
+    public static void main(String[] args) {
+        String s = "()";
+        int cmax =0, cmin =0;
+        for (int i = 0; i < s.length() ; i++) {
+            if(s.charAt(i)=='('){
+                cmax++;
+                cmin++;
+            }else if(s.charAt(i)==')'){
+                cmax--;
+                cmin--;
+            }else if(s.charAt(i)=='*'){
+                cmax++;
+                cmin--;
+            }
+            if(cmax<0){
+                System.out.println(false);
+                break;
+            }
+            cmin=Math.max(cmin,0);
+
+        }
+        if(cmin==0){
+            System.out.println(true);
+
+        }
+
+    }
+}

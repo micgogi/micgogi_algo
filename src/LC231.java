0cc5cf027b1f0cf62a668ac96280be3c905ea7d9
micgogi <rahul.gogyani@gmail.com>
2020-04-19 12:04:14 +0530

    LC231

---
+++ src/LC231.java
@@ -0,0 +1,12 @@
+/**
+ * @author Micgogi
+ * on 4/19/2020  11:48 AM
+ * Rahul Gogyani
+ */
+public class LC231 {
+    public static void main(String[] args) {
+        int n = 17;
+
+        System.out.println(Integer.bitCount(n));
+    }
+}

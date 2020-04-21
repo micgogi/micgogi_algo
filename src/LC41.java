9171ec5f832214ffb8e8e0aa2f3803b89341c6d5
micgogi <rahul.gogyani@gmail.com>
2020-04-21 12:57:00 +0530

    LC41

---
+++ src/LC41.java
@@ -0,0 +1,28 @@
+/**
+ * @author Micgogi
+ * on 4/21/2020  12:45 PM
+ * Rahul Gogyani
+ */
+public class LC41 {
+    public static void main(String[] args) {
+        int a[] = {3,4,-1,1};
+        int n = a.length;
+        for (int i = 0; i <n ; i++) {
+            while (a[i]>0&&a[i]<=n&&a[a[i]-1]!=a[i]){
+                int temp =a[a[i]-1];;
+                a[a[i]-1]=a[i];
+                a[i]=temp;
+            }
+
+        }
+        for (int i = 0; i <n ; i++) {
+            if(a[i]!=i+1){
+                System.out.println(i+1);
+                break;
+            }
+        }
+
+    }
+
+
+}

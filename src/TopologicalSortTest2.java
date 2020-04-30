0cd2c4aedf3c01cae666621863ef1f5fe6983730
micgogi <rahul.gogyani@gmail.com>
2020-04-30 15:53:42 +0530

    Topological sort

--- src/TopologicalSortTest2.java
+++ src/TopologicalSortTest2.java
@@ -1,15 +1,13 @@
-package Graph;
-
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
 /**
  * @author Micgogi
- * on 4/30/2020  12:18 PM
+ * on 4/30/2020  3:24 PM
  * Rahul Gogyani
  */
-public class TopologicalSortTest {
+public class TopologicalSortTest2 {
     static class Graph{
         int v;
         LinkedList<Integer> adjList[];
@@ -20,40 +18,42 @@ public class TopologicalSortTest {
                 adjList[i] = new LinkedList<>();
             }
         }
-        public  void addEdge(int s, int d){
+        public void addEdge(int s, int d){
             adjList[s].add(d);
         }
     }
 
     public static void topologicalSort(int v, Graph g){
-        Stack<Integer> stack  = new Stack<>();
         boolean visited[] = new boolean[v];
+        Stack<Integer> stack= new Stack();
         for (int i = 0; i <v ; i++) {
             if(!visited[i]){
-                dfs(i,visited,stack,g);
+                dfs(i,g,stack,visited );
             }
 
         }
         while (!stack.isEmpty()){
             System.out.println(stack.pop());
         }
-
     }
-    public static void dfs(int i, boolean visited[], Stack<Integer> stack, Graph g){
-        visited[i]=true;
-        Integer a;
+
+    private static void dfs(int i, Graph g, Stack<Integer> stack, boolean[] visited) {
+
+        visited[i] = true;
         Iterator<Integer> iterator = g.adjList[i].iterator();
+        Integer a =0;
         while(iterator.hasNext()){
-            a=iterator.next();
+           a = iterator.next();
             if(!visited[a]){
-                dfs(a,visited,stack,g);
+                dfs(a,g,stack,visited);
             }
         }
-        stack.push( new Integer(i));
+        stack.push(new Integer(i));
     }
 
+
     public static void main(String[] args) {
-        Graph g = new Graph(6);
+  Graph g = new Graph(6);
         g.addEdge(5,2);
         g.addEdge(5,0);
         g.addEdge(4,0);
@@ -62,6 +62,6 @@ public class TopologicalSortTest {
         g.addEdge(3,1);
         topologicalSort(6,g);
 
-    }
 
+    }
 }

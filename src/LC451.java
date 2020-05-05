b8955f4a5d515977cb4a2bfaf73f3fe2aa1ff86d
micgogi <rahul.gogyani@gmail.com>
2020-05-05 15:23:36 +0530

    LC451

---
+++ src/LC451.java
@@ -0,0 +1,33 @@
+import com.sun.deploy.uitoolkit.impl.fx.AppletStageManager;
+import org.omg.CORBA.INTERNAL;
+
+import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author Micgogi
+ * on 5/5/2020  2:12 PM
+ * Rahul Gogyani
+ */
+public class LC451 {
+    public static void main(String[] args) {
+        String s = "aaaccc";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.putIfAbsent(c, 0);
+            map.put(c, map.get(c) + 1);
+        }
+        List<Character> sortList = new LinkedList<>(map.keySet());
+        Collections.sort(sortList,(key1,key2)->map.get(key2)-map.get(key1));
+        StringBuilder sb = new StringBuilder();
+        for(char key : sortList) {
+            int charCount = map.get(key);
+            while(charCount-- != 0) sb.append(key);
+        }
+        System.out.println(sb.toString());
+    }
+}

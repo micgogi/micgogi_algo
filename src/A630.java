d632ee2db245ee740652af52b932f6067d069e75
micgogi <rahul.gogyani@gmail.com>
2020-05-22 08:59:45 +0530

    A630

---
+++ src/A630.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Micgogi
+ * on 5/22/2020  8:54 AM
+ * Rahul Gogyani
+ */
+public class A630 {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String s = br.readLine();
+            System.out.println(25);
+        }catch (Exception e){
+
+        }
+    }
+}

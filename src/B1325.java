23aa3002f5440aa2bbead0ee5c53c30cec83f7b4
micgogi <rahul.gogyani@gmail.com>
2020-05-22 08:51:20 +0530

    B1325

---
+++ src/B1325.java
@@ -0,0 +1,35 @@
+import org.omg.CORBA.INTERNAL;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Micgogi
+ * on 5/22/2020  8:40 AM
+ * Rahul Gogyani
+ */
+public class B1325 {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int t = Integer.parseInt(br.readLine());
+            while(t--!=0){
+                int n = Integer.parseInt(br.readLine());
+                String s[] = br.readLine().split(" ");
+                int a[] = new int[n];
+                for (int i = 0; i <a.length ; i++) {
+                    a[i]=Integer.parseInt(s[i]);
+                }
+                Set<Integer> set = new HashSet<>();
+                for (int i = 0; i <a.length ; i++) {
+                    set.add(a[i]);
+                }
+                System.out.println(set.size());
+            }
+        }catch (Exception e){
+
+        }
+    }
+}

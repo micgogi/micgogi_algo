09a7a436f250541b26adb6d3ea6b7f3517bfd034
micgogi <rahul.gogyani@gmail.com>
2020-05-24 16:05:47 +0530

    LC1008

---
+++ src/C431.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Micgogi
+ * on 5/24/2020  12:56 PM
+ * Rahul Gogyani
+ */
+public class C431 {
+    final static double MOD = 1e9+7;
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String s[] = br.readLine().split( " ");
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int d = Integer.parseInt(s[2]);
+            System.out.println(MOD);
+
+
+        }catch (Exception e){
+
+        }
+    }
+}
---
+++ src/LCMayChallenge/LC1008.java
@@ -0,0 +1,35 @@
+package LCMayChallenge;
+
+/**
+ * @author Micgogi
+ * on 4/20/2020  2:34 PM
+ * Rahul Gogyani
+ */
+public class LC1008 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        int a[] = {8,5,1,7,10,12};
+        Node n = construct(a, Integer.MAX_VALUE);
+
+    }
+   static int index =0;
+
+    public static Node construct(int a[], int limit){
+        if(index==a.length||a[index]>limit){
+            return null;
+
+        }
+
+        Node root = new Node(a[index++]);
+        root.left= construct(a,root.data);
+        root.right = construct(a, limit);
+        return root;
+    }
+}

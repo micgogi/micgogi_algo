e3b88eaed62779759780611c46d42c502744908e
micgogi <rahul.gogyani@gmail.com>
2020-05-26 11:18:25 +0530

    LC72

--- src/LC72.java
+++ src/LC72.java
@@ -21,7 +21,7 @@ public class LC72 {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i][j]) + 1;
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i-1][j-1]) + 1;
                 }
             }
         }

4f4558e92fab4134769247f561af7b75ded2a393
micgogi <rahul.gogyani@gmail.com>
2020-06-04 13:36:56 +0530

    REVERSE STRING LC344JUNE4

---
+++ src/LCJuneChallenge/LC344.java
@@ -0,0 +1,22 @@
+package LCJuneChallenge;/*
+ *@author Rahul Gogyani
+ *
+ * 3:46 PM 2/13/2020
+ */
+
+import java.util.Arrays;
+
+public class LC344 {
+    public static void main(String[] args) {
+        char c[] = {'h','e','l','l','o'};
+        for (int i = 0; i <c.length/2 ; i++) {
+            char temp = c[i];
+            c[i]=c[c.length-1-i];
+            c[c.length-i-1]=temp;
+
+        }
+        System.out.println(Arrays.toString(c));
+
+
+    }
+}

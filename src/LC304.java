a721c0e4be0c540c1513a4dc9e70019aa29806b7
micgogi <rahul.gogyani@gmail.com>
2020-06-11 13:00:55 +0530

    LC304

---
+++ src/LC304.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * @author Micgogi
+ * on 6/11/2020  12:40 PM
+ * Rahul Gogyani
+ */
+public class LC304 {
+    public static void main(String[] args) {
+        int matrix[][] = {
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}};
+    int sum[][] = new int[matrix.length+1][matrix[0].length+1];
+        for (int i = 1; i <=matrix.length ; i++) {
+            for (int j = 1; j <=matrix[0].length ; j++) {
+                sum[i][j]= sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+matrix[i-1][j-1];
+            }
+        }
+        int r1 =2,c1=1,r2=4,c2=3;
+        r1++;c1++;r2++;c2++;
+        System.out.println(Arrays.deepToString(sum));
+        System.out.println(sum[r2][c2]-sum[r2][c1-1]-sum[r1-1][c2]+sum[r1-1][c1-1]);
+    }
+
+}

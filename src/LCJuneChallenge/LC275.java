096ce9c57d236ca9334ea78263ae8278ae5d5e8c
micgogi <rahul.gogyani@gmail.com>
2020-06-18 13:42:45 +0530

    LC275

---
+++ src/LCJuneChallenge/LC275.java
@@ -0,0 +1,27 @@
+package LCJuneChallenge;
+
+/**
+ * @author Micgogi
+ * on 6/18/2020  1:34 PM
+ * Rahul Gogyani
+ */
+public class LC275 {
+    public static void main(String[] args) {
+
+        int a[] = {0, 1, 3, 5, 6};
+        int left = 0;
+        int right = a.length-1;
+        int n = a.length;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(a[mid]==n-mid){
+                System.out.println(n-mid);
+                return;
+            }else if(a[mid]<n-mid){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+    }
+}

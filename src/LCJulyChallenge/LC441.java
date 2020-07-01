93599f74f65315e34f99aef3c94e22f689c9cc7a
micgogi <rahul.gogyani@gmail.com>
2020-07-01 13:18:23 +0530

    LC441

---
+++ src/LCJulyChallenge/LC441.java
@@ -0,0 +1,36 @@
+package LCJulyChallenge;
+
+import java.util.Scanner;
+
+/**
+ * @author Micgogi
+ * on 7/1/2020  12:54 PM
+ * Rahul Gogyani
+ */
+public class LC441 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       int left =0;
+       int right = n;
+       long mid;
+       long curr;
+       while(left<=right){
+           mid =(int)(left+(right-left)/2);
+           curr = mid*(mid+1)/2;
+           if(curr==n){
+               System.out.println(mid);
+               return;
+           }
+           if(n<curr){
+               right = (int)mid-1;
+
+           }else{
+               left = (int) mid+1;
+           }
+       }
+        System.out.println(right);
+    }
+
+
+}

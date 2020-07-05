2d48369a6cab1792f5d8379ce40022ab208828a8
micgogi <rahul.gogyani@gmail.com>
2020-07-05 15:06:09 +0530

    LC461

---
+++ src/LCJulyChallenge/LC461.java
@@ -0,0 +1,25 @@
+package LCJulyChallenge;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Micgogi
+ * on 7/5/2020  3:01 PM
+ * Rahul Gogyani
+ */
+public class LC461 {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String s[] = br.readLine().split(" ");
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
+            int t = x^y;
+            System.out.println(Integer.bitCount(t));
+        }catch (Exception e){
+
+        }
+    }
+
+}

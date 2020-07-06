bf0bb070f7a38d448d87b440917935970051b6a6
micgogi <rahul.gogyani@gmail.com>
2020-07-06 12:49:38 +0530

    LC66

---
+++ src/LCJulyChallenge/LC66.java
@@ -0,0 +1,32 @@
+package LCJulyChallenge;
+
+import java.util.Arrays;
+
+/**
+ * @author Micgogi
+ * on 7/6/2020  12:44 PM
+ * Rahul Gogyani
+ */
+public class LC66 {
+    public static void main(String[] args) {
+        int a[] = {9,9,9};
+        int carry =0;
+        for (int i = a.length-1; i>=0 ; i--) {
+            if(a[i]==9){
+                a[i]=0;
+                carry++;
+                if(carry==a.length){
+                    int b[] = new int[a.length+1];
+                    b[0] = 1;
+                    System.out.println(Arrays.toString(b));
+                    return;
+                }
+            }else{
+                a[i]=a[i]+1;
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
+}

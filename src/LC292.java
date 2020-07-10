f4bcc7fb4413aaa75d726da4a8237b21ef871929
micgogi <rahul.gogyani@gmail.com>
2020-07-10 19:02:13 +0530

    LC292

---
+++ src/LC292.java
@@ -0,0 +1,12 @@
+/**
+ * @author Micgogi
+ * on 7/10/2020  7:01 PM
+ * Rahul Gogyani
+ */
+public class LC292 {
+    public static void main(String[] args) {
+        int n= 5;
+        System.out.println(n%4!=0);
+    }
+
+}

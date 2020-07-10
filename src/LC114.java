181f1a7d060d2c9472ce71b34d1e7692a3f2be87
micgogi <rahul.gogyani@gmail.com>
2020-07-10 17:27:38 +0530

    LC114

---
+++ src/LC114.java
@@ -0,0 +1,41 @@
+/**
+ * @author Micgogi
+ * on 7/10/2020  5:13 PM
+ * Rahul Gogyani
+ */
+public class LC114 {
+
+    static TreeNode root;
+    public static class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+        flatten(root);
+
+
+    }
+    static TreeNode prev = null;
+    public static void flatten(TreeNode root){
+        if(root==null)return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}

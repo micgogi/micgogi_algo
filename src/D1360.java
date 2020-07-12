f9832c5a9f146379324eba6aee72e0c93b42ae75
micgogi <rahul.gogyani@gmail.com>
2020-07-12 12:17:21 +0530

    D1360

---
+++ src/D1360.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/12/2020  10:23 AM
+ * Rahul Gogyani
+ */
+public class D1360 {
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int ans = n;
+            for (int i = 1; i*i <=n ; i++) {
+                if(n%i==0){
+                    if(i<=k){
+                        ans = Math.min(ans,n/i);
+                    }
+                    if(n/i<=k){
+                        ans = Math.min(ans,i);
+                    }
+                }
+            }
+            System.out.println(ans);
+
+
+        }
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

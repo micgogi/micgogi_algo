de6da3a335b9071c11ed0c9b44857a6ac88e14d7
micgogi <rahul.gogyani@gmail.com>
2020-07-14 15:38:47 +0530

    A1343

---
+++ src/A1343.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/14/2020  3:16 PM
+ * Rahul Gogyani
+ */
+public class A1343 {
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t--!=0){
+            int n = sc.nextInt();
+            int ans =0;
+            for (int i = 2; i <30 ; i++) {
+                int  d= (1<<i)-1;
+                if(n%d==0){
+                    ans=n/d;
+                    break;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

e9b3ee2270de526496f7847ca4c83cd15d78fd50
micgogi <rahul.gogyani@gmail.com>
2020-07-14 15:50:03 +0530

    B1343

---
+++ src/B1343.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/14/2020  3:45 PM
+ * Rahul Gogyani
+ */
+public class B1343 {
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while (t--!=0){
+            int n = sc.nextInt();
+            if(n%4!=0){
+                System.out.println("NO");
+                continue;
+            }
+            System.out.println("YES");
+             n = n/2;
+            for (int i = 1; i <=n ; i++) {
+                System.out.print(2*i+" ");
+            }
+            for (int i = 1; i <n ; i++) {
+                System.out.print(2*i-1+" ");
+            }
+            System.out.println(3*n-1);
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

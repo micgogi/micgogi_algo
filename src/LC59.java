196b26ef1d6c835f2aada753d934f5dde0f012e7
micgogi <rahul.gogyani@gmail.com>
2020-07-15 11:10:53 +0530

    LC59

---
+++ src/LC59.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/15/2020  10:43 AM
+ * Rahul Gogyani
+ */
+public class LC59 {
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int n = 3;
+        int matrix[][] = new int[n][n];
+        int rowBegin = 0;
+        int rowEnd = matrix.length-1;
+        int colBegin =0;
+        int colEnd = matrix[0].length-1;
+        int num=1;
+        while (rowBegin<=rowEnd&&colBegin<=colEnd){
+            for (int i = colBegin; i <=colEnd ; i++,num++) {
+                matrix[rowBegin][i] = num;
+            }
+            rowBegin++;
+            for (int i =rowBegin; i <=rowEnd ; i++,num++) {
+                matrix[i][colEnd] = num;
+            }
+            colEnd--;
+            if(rowBegin<=rowEnd) {
+                for (int i =colEnd; i>=colBegin; i--,num++) {
+                    matrix[rowEnd][i]= num;
+                }
+            }
+            rowEnd--;
+            if(colBegin<=colEnd){
+                for (int i = rowEnd; i >=rowBegin ; i--,num++) {
+                    matrix[i][colBegin] = num;
+                }
+            }
+            colBegin++;
+        }
+        System.out.println(Arrays.deepToString(matrix));
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

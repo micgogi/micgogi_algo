8d51a09ea866d714c54e8ae7b8a5c42d2e16ec56
micgogi <rahul.gogyani@gmail.com>
2020-07-18 15:10:51 +0530

    LC210

--- src/Graph/TopologicalSortTest.java
+++ src/Graph/TopologicalSortTest.java
@@ -53,14 +53,13 @@ public class TopologicalSortTest {
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph(6);
-        g.addEdge(5,2);
-        g.addEdge(5,0);
-        g.addEdge(4,0);
-        g.addEdge(4,1);
+        Graph g = new Graph(4);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
         g.addEdge(2,3);
-        g.addEdge(3,1);
-        topologicalSort(6,g);
+
+        topologicalSort(4,g);
 
     }
 
---
+++ src/LCJulyChallenge/LC210.java
@@ -0,0 +1,68 @@
+package LCJulyChallenge;
+
+import java.util.*;
+
+/**
+ * @author Micgogi
+ * on 7/18/2020  1:30 PM
+ * Rahul Gogyani
+ */
+public class LC210 {
+   static HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+
+    public static void addEdge(int src, int dest){
+        adjList.putIfAbsent(src,new ArrayList<>());
+        List<Integer> list = adjList.get(src);
+        list.add(dest);
+        adjList.put(src,list);
+        inDegree[dest]++;
+
+    }
+    static int[] inDegree;
+    public static void main(String args[]) {
+    int a[][] = {{1,0}};
+    int n =2;
+       inDegree = new int[n];
+        for (int i = 0; i <a.length ; i++) {
+            addEdge(a[i][1],a[i][0]);
+        }
+        System.out.println(adjList);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int j = 0; j < n; j++) {
+            if (inDegree[j] == 0) {
+                queue.add(j);
+            }
+        }
+        int t=0;
+        int ans[] = new int[n];
+        while (!queue.isEmpty()){
+            int s = queue.poll();
+            ans[t++] = s;
+
+            if(adjList.get(s)!=null) {
+                List<Integer> list = adjList.get(s);
+                for (int i = 0; i <list.size() ; i++) {
+                   inDegree[list.get(i)]--;
+                    if(inDegree[list.get(i)]==0){
+                        queue.add(list.get(i));
+                    }
+                }
+
+            }
+
+
+
+
+        }
+        if(t==n){
+            System.out.println(Arrays.toString(ans));
+            return;
+        }
+    }
+
+
+
+
+
+}
+

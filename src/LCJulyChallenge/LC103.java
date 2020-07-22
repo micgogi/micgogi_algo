4c05c1a2523af400b96288270e19e96d9675a151
micgogi <rahul.gogyani@gmail.com>
2020-07-22 12:43:42 +0530

    LC103

---
+++ src/LCJulyChallenge/LC103.java
@@ -0,0 +1,103 @@
+package LCJulyChallenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * @author Micgogi
+ * on 7/18/2020  11:44 AM
+ * Rahul Gogyani
+ */
+public class LC103 {
+    static TreeNode root;
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+      //  int t = sc.nextInt();
+        root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(bfs(root));
+    }
+    public static List<List<Integer>> bfs(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        queue.add(root);
+        boolean flag = false;
+        while (!queue.isEmpty()){
+
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+
+            while (size--!=0){
+                TreeNode node = queue.poll();
+                if (flag){
+                    list.add(0,node.val);
+                }else {
+                    list.add(node.val);
+                }
+                if(node.left!=null)queue.add(node.left);
+                if(node.right!=null)queue.add(node.right);
+            }
+            res.add(list);
+            flag = !flag;
+        }
+
+        return res;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

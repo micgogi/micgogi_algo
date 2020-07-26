b965fea7ee3bcadf1a258a6fe457f1b074d92a0b
micgogi <rahul.gogyani@gmail.com>
2020-07-26 16:58:57 +0530

    🤣 LC796🤣

---
+++ src/LC796.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/26/2020  4:49 PM
+ * Rahul Gogyani
+ */
+public class LC796 {
+    public static void main(String args[]) {
+       String A = "akinta";
+       String B = "kintaa";
+        System.out.println(rotateString(A,B));
+
+
+    }
+    public static boolean rotateString(String A, String B) {
+       return A.length()==B.length() && (A+A).contains(B);
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+

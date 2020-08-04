2bbfed86b4fb9c89158a01fd6febc8f934daa314
micgogi <rahul.gogyani@gmail.com>
2020-08-04 14:03:28 +0530

    LC342

---
+++ src/LCAugustChallenge/LC342.java
@@ -0,0 +1,23 @@
+package LCAugustChallenge;
+
+/**
+ * @author Micgogi
+ * on 4/19/2020  12:30 PM
+ * Rahul Gogyani
+ */
+public class LC342 {
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowerOfFour(n));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}

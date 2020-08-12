df1aee9bbbb81cdaf1335e37f126873082ac7fee
micgogi <rahul.gogyani@gmail.com>
2020-08-12 14:44:27 +0530

    LC119

---
+++ src/LCAugustChallenge/LC119.java
@@ -0,0 +1,37 @@
+package LCAugustChallenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Micgogi
+ * on 3/14/2020  4:51 PM
+ * Rahul Gogyani
+ */
+public class LC119 {
+    public static void main(String[] args) {
+        int num = 5;
+        int k=2;
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+        lists.add(a);
+        for (int i = 1; i <=num ; i++) {
+            List<Integer> list = new ArrayList<>();
+            List<Integer> prev=null;
+
+            prev = lists.get(i-1);
+
+
+            for (int j = 0; j <=i ; j++) {
+                if(j==0||j==i){
+                    list.add(1);
+                }else{
+                    list.add(prev.get(j-1)+prev.get(j));
+                }
+            }
+            lists.add(list);
+        }
+        System.out.println(lists.get(k));
+    }
+}

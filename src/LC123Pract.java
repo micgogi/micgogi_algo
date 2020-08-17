a81dde42f9e0930ceed1994859d8fe01ac0ddc74
micgogi <rahul.gogyani@gmail.com>
2020-08-17 16:41:05 +0530

    LC123

--- src/LC123Pract.java
+++ src/LC123Pract.java
@@ -10,8 +10,26 @@ import java.util.StringTokenizer;
  */
 public class LC123Pract {
     public static void main(String args[]) {
-
+        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        System.out.println(maxProfit(prices));
 
     }
+
+    public static int maxProfit(int[] prices) {
+        int k =2;
+        int dp[][] = new int[3][prices.length];
+        for (int i = 1; i <=2 ; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j <prices.length ; j++) {
+
+                dp[i][j] = Math.max(dp[i][j-1],maxDiff+prices[j]);
+                maxDiff = Math.max(maxDiff,dp[i-1][j]-prices[j]);
+            }
+
+        }
+        return dp[k][prices.length-1];
+    }
+
+
 }
 

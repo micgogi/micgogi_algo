377b8305f2d157671e16896b3e203331ed89c3b1
micgogi <rahul.gogyani@gmail.com>
2020-08-20 12:50:36 +0530

    LC143

--- src/LCAugustChallenge/LC143.java
+++ src/LCAugustChallenge/LC143.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+package LCAugustChallenge;
 
 /**
  * @author Micgogi

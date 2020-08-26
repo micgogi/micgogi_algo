531c6def553a284e786ce44da6e3138e73ef72d9
micgogi <rahul.gogyani@gmail.com>
2020-08-26 12:30:58 +0530

    C1401

--- src/Test.java
+++ src/Test.java
@@ -1,43 +1,58 @@
+import java.lang.annotation.*;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Micgogi
  * on 11/18/2019  2:17 AM
  * 65 78 75 73 84 65
  */
 public class Test {
+
     public static void main(String[] args) {
-        String s = "011111111111111";
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        int j = 1;
-        int min = Integer.MIN_VALUE;
-        boolean found = true;
-        while (j < s.length()) {
-            if (s.charAt(i) == '0' && s.charAt(j) == '1'&&found) {
-                j++;
-                found = true;
-                if (j - i > min&&found) {
-                    min = j - i;
 
-                }
-                continue;
-            }  else if (s.charAt(i) == '0'&&s.charAt(j)=='0') {
-                i = j;
-                j = j + 1;
-                found = false;
-            }else{
-                i++;
-                j++;
-                if (s.charAt(i) == '0' && s.charAt(j) == '1') {
-
-                    found = true;
-
-                }
-            }
+//        String regex = ".@.";
+//        compare("HackerEarth@jfjf.ocm",regex);
+//        compare("a@N",regex);
+//        compare("Java@Program",regex);
 
 
-
-
-        }
-        System.out.println(min);
     }
+//    public static void compare(String str, String reg){
+//        if(str.matches(reg)){
+//            System.out.println(str+"matches");
+//        }else{
+//            System.out.println(str+"doesnotmatch");
+//        }
+//    }
+
+
+//    @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+//    @interface MyAnnotation{}
+//
+//    @Retention(RetentionPolicy.RUNTIME)
+//    @interface Hints{
+//        Hint[] value();
+//    }
+//    @Repeatable(Hints.class)
+//    @Retention(RetentionPolicy.RUNTIME)
+//    @interface Hint
+//    {
+//        String value();
+//    }
+//    @Hint("hint1")
+//    @Hint("hint2")
+//
+//    class Person{}
+//    public static void main(String[] args){
+//        Hint hint = Person.class.getAnnotation(Hint.class);
+//        System.out.println(hint);
+//        Hints hints1 = Person.class.getAnnotation(Hints.class);
+//        System.out.println(hints1.value().length);
+//        Hint[] hints2 = Person.class.getAnnotationsByType(Hint.class);
+//        System.out.println(hints2.length);
+//
+//    }
+    // null 2 2
+
 }

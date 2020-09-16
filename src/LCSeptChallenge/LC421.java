fcf1b320d714b882147e0ae32e1d6c8f4119c524
micgogi <rahul.gogyani@gmail.com>
2020-09-16 18:16:37 +0530

    LC421

---
+++ src/LCSeptChallenge/LC421.java
@@ -0,0 +1,35 @@
+package LCSeptChallenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Micgogi
+ * on 9/16/2020  5:18 PM
+ * Rahul Gogyani
+ */
+public class LC421 {
+    public static void main(String args[]) {
+        int nums[] = {25, 10, 2, 8, 5, 3};
+        int max = 0;
+        int mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            int newMask = max | (1 << i);
+            for (int prefix : set) {
+                if (set.contains(newMask ^ prefix)) {
+                    max = newMask;
+                    break;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+
+}
+

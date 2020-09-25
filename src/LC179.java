9377d896abf7c3503d41fc693a4781d28021281c
micgogi <rahul.gogyani@gmail.com>
2020-09-25 15:47:35 +0530

    LC179

---
+++ src/LC179.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * @author Micgogi
+ * on 9/25/2020  3:30 PM
+ * Rahul Gogyani
+ */
+public class LC179 {
+    public static void main(String args[]) {
+        int[] nums = {65, 78, 75, 73, 84, 65};
+        String[] st = new String[nums.length];
+        for (int i = 0; i < st.length; i++) {
+            st[i] = "" + nums[i];
+        }
+        Arrays.sort(st, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        StringBuilder sb = new StringBuilder();
+        for (String num: st){
+            sb.append(num);
+        }
+        if(sb.charAt(0)=='0'){
+            System.out.println(0);
+            return;
+        }
+        System.out.println(sb.toString());
+    }
+
+}
+

c18db4863fcafc5bacc45325d9e21d6e9d47b869
micgogi <rahul.gogyani@gmail.com>
2020-10-03 13:28:21 +0530

    LC532

---
+++ src/LC532.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * @author Micgogi
+ * on 10/3/2020  1:06 PM
+ * Rahul Gogyani
+ */
+public class LC532 {
+    public static void main(String args[]) {
+        int a[] = {1, 3, 1, 5, 4};
+
+        int k = 0;
+        System.out.println(findPairs(a, k));
+    }
+
+    public static int findPairs(int[] nums, int k) {
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        int count = 0;
+        for (int key : counter.keySet()) {
+            if (k == 0) {
+                if (counter.get(key) >= 2) count++;
+            } else {
+                if (counter.containsKey(key + k)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+}
+

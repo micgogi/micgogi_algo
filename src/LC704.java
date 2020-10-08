1b2111261afcdbe6009e018430b9e3054698d1bc
micgogi <rahul.gogyani@gmail.com>
2020-10-08 13:33:37 +0530

    LC704

---
+++ src/LC704.java
@@ -0,0 +1,28 @@
+/**
+ * @author Micgogi
+ * on 10/8/2020  1:22 PM
+ * Rahul Gogyani
+ */
+public class LC704 {
+    public static void main(String args[]) {
+        int a[] = {65, 78, 75, 73, 84, 65};
+        int target = 65;
+        System.out.println(search(a, target));
+    }
+
+    public static int search(int[] a, int target) {
+        int left = 0, right = a.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (a[mid] == target) return mid;
+            if (a[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+

600db702fa203d7a5c602113ad6e49d6ca4bf5e9
Rahul Gogyani <rahul.gogyani@gmail.com>
2020-10-12 11:21:56 +0530

    LC1551

---
+++ src/LC1551.java
@@ -0,0 +1,9 @@
+class LC1552 {
+    public static void main(String[] args){
+      int n = 6;
+      System.out.println(minOperations(n));
+    }
+    public static int minOperations(int n) {
+     return n/2*(n/2+n%2);
+    }
+}

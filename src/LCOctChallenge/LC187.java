77f4fed836b8d278665493eff05c8b13aea782d5
micgogi <rahul.gogyani@gmail.com>
2020-10-17 15:34:59 +0530

    LC187

---
+++ src/LCOctChallenge/LC187.java
@@ -0,0 +1,35 @@
+package LCOctChallenge;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Micgogi
+ * on 10/17/2020  3:26 PM
+ * Rahul Gogyani
+ */
+public class LC187 {
+    public static void main(String args[]) {
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        System.out.println(findRepeatedDnaSequences(s));
+    }
+    public static List<String> findRepeatedDnaSequences(String s) {
+        int start = 0;
+        int end = 10;
+        Set<String> set = new HashSet<>();
+        Set<String> rep = new HashSet<>();
+        while (end <= s.length()){
+            String t = s.substring(start, end);
+            if(set.contains(t)){
+                rep.add(t);
+            }
+            set.add(t);
+            start++;
+            end++;
+        }
+        return new ArrayList<>(rep);
+    }
+}
+

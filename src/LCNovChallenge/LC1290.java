0e129c8e3146f821758b95c51925f15c99d5041e
micgogi <rahul.gogyani@gmail.com>
2020-11-01 12:58:30 +0530

    LC1290

--- src/LCNovChallenge/LC1290.java
+++ src/LCNovChallenge/LC1290.java
@@ -1,87 +1,29 @@
 package LCNovChallenge;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 /**
  * @author Micgogi
  * on 11/1/2020  12:56 PM
  * Rahul Gogyani
  */
 public class LC1290 {
-    public static void main(String args[]) {
-        FastReader sc = new FastReader();
-        int t = sc.nextInt();
+    static Node head;
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data= data;
+        }
     }
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(0);
+        head.next.next = new Node(1);
+        StringBuilder sb = new StringBuilder();
+        while(head!=null){
+            sb.append(head.data);
+            head = head.next;
         }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-        int[] readArray(int n) {
-            int[] a = new int[n];
-            for (int i = 0; i < n; i++) a[i] = nextInt();
-            return a;
-        }
-
-        int[][] read2DArray(int n) {
-            int[][] a = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = nextInt();
-                }
-            }
-            return a;
-        }
-
-        int[][] read2DArray(int n, int m) {
-            int[][] a = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    a[i][j] = nextInt();
-                }
-            }
-            return a;
-        }
+        System.out.println(Integer.parseInt(sb.toString(),2));
     }
 
 }

87c45f9229ea0496a3850ea35edfa878ddd1e200
micgogi <rahul.gogyani@gmail.com>
2020-11-25 18:50:25 +0530

    LC1015

---
+++ src/LC1015.java
@@ -0,0 +1,20 @@
+/**
+ * @author Micgogi
+ * on 11/25/2020  6:45 PM
+ * Rahul Gogyani
+ */
+public class LC1015 {
+    public static void main(String args[]) {
+        System.out.println(smallestRepunitDivByK(111));
+    }
+
+    public static int smallestRepunitDivByK(int k) {
+        int rem = 0;
+        for (int i = 1; i <= k; i++) {
+            rem = (rem * 10 + 1) % k;
+            if (rem == 0) return i;
+        }
+        return -1;
+    }
+}
+

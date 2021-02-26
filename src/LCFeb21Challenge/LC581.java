c9a8c50c5dc08394ac9444501f8d670b8ee4bb80
micgogi <rahul.gogyani@gmail.com>
2021-02-26 09:39:46 +0530

    LC581

---
+++ src/LCFeb21Challenge/LC581.java
@@ -0,0 +1,103 @@
+package LCFeb21Challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 2/26/2021  9:36 AM
+ * Rahul Gogyani
+ */
+public class LC581 {
+    public static void main(String args[]) {
+        int a[] = {65, 78, 75, 73, 84, 65};
+        System.out.println(findUnsortedSubarray(a));
+    }
+
+    public static int findUnsortedSubarray(int[] nums) {
+        int b[] = nums.clone();
+        Arrays.sort(b);
+        int left = nums.length;
+        int right = 0;
+        for (int i = 0; i < b.length; i++) {
+            if (nums[i] != b[i]) {
+                left = Math.min(left, i);
+                right = Math.max(right, i);
+            }
+        }
+        return right - left >= 0 ? right - left + 1 : 0;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[][] read2DArray(int n) {
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        int[][] read2DArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+    }
+
+}
+

d2b8cb2bcfe73be376300c89e199f01d0ac2f15c
micgogi <rahul.gogyani@gmail.com>
2021-03-08 18:13:23 +0530

    LC1332

---
+++ src/LC1332.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * @author Micgogi
+ * on 3/8/2021  6:11 PM
+ * Rahul Gogyani
+ */
+public class LC1332 {
+    public static void main(String args[]) {
+        System.out.println(removePalindromeSub("ababa"));
+    }
+
+    public static int removePalindromeSub(String s) {
+        if (s.isEmpty()) return 0;
+        else if (new StringBuilder(s).reverse().toString().equals(s)) return 1;
+        else {
+            return 2;
+        }
+    }
+
+}
+

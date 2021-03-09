85ddeadb0e3714a31933151bed392bf140133bf4
micgogi <rahul.gogyani@gmail.com>
2021-03-09 20:08:49 +0530

    LC623

---
+++ src/March21Challenge/LC623.java
@@ -0,0 +1,142 @@
+package March21Challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 3/9/2021  7:35 PM
+ * Rahul Gogyani
+ */
+public class LC623 {
+    static TreeNode root;
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]) {
+        root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(1);
+        root.right.left = new TreeNode(5);
+        int v = 1;
+        int d = 2;
+        addOneRow(root, v, d);
+        printTree(root);
+
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root != null) {
+            System.out.println(root.data);
+            printTree(root.left);
+            printTree(root.right);
+        }
+    }
+
+    public static TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode newNode = new TreeNode(v);
+            newNode.left = root;
+            return newNode;
+        }
+        dfs(root, 1, v, d);
+        return root;
+    }
+
+    public static void dfs(TreeNode root, int depth, int v, int d) {
+        if (root == null) return;
+        if (depth == d - 1) {
+            TreeNode temp = root.left;
+            root.left = new TreeNode(v);
+            root.left.left = temp;
+            TreeNode temp1 = root.right;
+            root.right = new TreeNode(v);
+            root.right.right = temp1;
+        } else {
+            dfs(root.left, depth + 1, v, d);
+            dfs(root.right, depth + 1, v, d);
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[][] read2DArray(int n) {
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        int[][] read2DArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+    }
+
+}
+

0967ee4b369bf7a26bcb1eb79c2260adb6f3ba4d
micgogi <rahul.gogyani@gmail.com>
2021-04-13 11:07:36 +0530

    LC667

---
+++ src/LCApr21Challenge/LC667.java
@@ -0,0 +1,43 @@
+package LCApr21Challenge;
+
+import java.util.Arrays;
+
+/**
+ * @author Micgogi
+ * on 4/13/2021  10:43 AM
+ * Rahul Gogyani
+ */
+public class LC667 {
+    public static void main(String args[]) {
+        System.out.println(Arrays.toString(constructArray(5, 3)));
+    }
+
+    public static int[] constructArray(int n, int k) {
+        int[] result = new int[n];
+        int high = n;
+        int low = 1;
+        int i = 0;
+        result[i++] = low;
+        boolean isHigh = false;
+        while (k > 1) {
+            result[i++] = high--;
+            k--;
+            isHigh = true;
+            if (k > 1) {
+                result[i++] = low++;
+                k--;
+                isHigh = false;
+            }
+        }
+        while (i < n) {
+            if (isHigh) {
+                result[i++] = high--;
+            } else {
+                result[i++] = low--;
+            }
+        }
+        return result;
+    }
+
+}
+

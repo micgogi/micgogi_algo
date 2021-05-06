0731a7acf52ca6a1f3574fbbd7462a5d0cf0f942
micgogi <rahul.gogyani@gmail.com>
2021-05-06 19:23:21 +0530

    LC109

---
+++ src/LC21MayChallenge/LC109.java
@@ -0,0 +1,64 @@
+package LC21MayChallenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 5/6/2021  7:21 PM
+ * Rahul Gogyani
+ */
+public class LC109 {
+    static ListNode head;
+    static public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        head = new ListNode(-10);
+        head.next = new ListNode(-3);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(5);
+        head.next.next.next.next = new ListNode(9);
+        TreeNode temp = sorted(head);
+        printTree(temp);
+
+    }
+    public static void printTree(TreeNode temp){
+        if(temp==null)return;
+
+
+        System.out.println(temp.val);
+        printTree(temp.left);
+        printTree(temp.right);
+    }
+    public static TreeNode sorted(ListNode head){
+        if(head==null)return null;
+        if(head.next==null)return new TreeNode(head.val);
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while(fast!=null&&fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast= fast.next.next;
+        }
+        prev.next=null;
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sorted(head);
+        root.right = sorted(slow.next);
+        return root;
+    }
+}
+
+

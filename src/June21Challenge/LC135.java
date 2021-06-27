199845245cca0e73bf9e8281e19fcb4718cd0737
micgogi <rahul.gogyani@gmail.com>
2021-06-27 13:11:27 +0530

    LC135

---
+++ src/June21Challenge/LC135.java
@@ -0,0 +1,123 @@
+package June21Challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 6/27/2021  12:57 PM
+ * Rahul Gogyani
+ */
+public class LC135 {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[][] read2DArray(int n) {
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        int[][] read2DArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+    }
+
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        int a[] = new int[t];
+        for (int i = 0; i < t; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println(candy(a));
+    }
+
+    public static int candy(int[] ratings) {
+        if (ratings.length == 1) return 1;
+        int left[] = new int[ratings.length];
+        int right[] = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        int ans = 0;
+        for (int i = 0; i < ratings.length; i++) {
+            ans += Math.max(left[i], right[i]);
+        }
+        return ans;
+
+    }
+
+
+
+}
+

2e514e01da16a7259b3e69286123827291216690
micgogi <rahul.gogyani@gmail.com>
2021-07-16 10:12:17 +0530

    LC611

--- src/july21challenge/LC791.java
+++ src/july21challenge/LC791.java
@@ -12,19 +12,20 @@ import java.util.StringTokenizer;
  */
 public class LC791 {
     public static void main(String args[]) {
-        System.out.println(customSortString("cba","abcd"));
+        System.out.println(customSortString("cba", "abcd"));
     }
+
     public static String customSortString(String order, String str) {
         int[] count = new int[26];
-        for (char c: str.toCharArray())count[c-'a']++;
+        for (char c : str.toCharArray()) count[c - 'a']++;
         StringBuilder sb = new StringBuilder();
-        for (char c:order.toCharArray()){
-            while (count[c-'a']-->0){
+        for (char c : order.toCharArray()) {
+            while (count[c - 'a']-- > 0) {
                 sb.append(c);
             }
         }
-        for (char c='a'; c<='z';c++){
-            while (count[c-'a']-->0){
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (count[c - 'a']-- > 0) {
                 sb.append(c);
             }
         }

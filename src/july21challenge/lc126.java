169210fb020047bc0643c320309f339f9d5299c5
micgogi <rahul.gogyani@gmail.com>
2021-07-26 16:49:12 +0530

    LC108

---
+++ src/july21challenge/lc126.java
@@ -0,0 +1,25 @@
+package july21challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 7/25/2021  4:06 PM
+ * Rahul Gogyani
+ */
+public class lc126 {
+
+
+
+    
+    public static void main(String args[]) {
+
+
+    }
+
+
+}
+

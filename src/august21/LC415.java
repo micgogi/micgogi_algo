620a6af76e5b3c5faa66da331f600c82313eb78c
micgogi <rahul.gogyani@gmail.com>
2021-08-09 14:07:59 +0530

    LC415

---
+++ src/august21/LC415.java
@@ -0,0 +1,51 @@
+package august21;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 8/9/2021  1:45 PM
+ * Rahul Gogyani
+ */
+public class LC415 {
+    public static void main(String[] args) {
+        String s1 = "8265728576";
+        String s2 = "657875738465";
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        for (int i = 0; i < s1.length(); i++) {
+            stack1.push(Integer.parseInt("" + s1.charAt(i)));
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            stack2.push(Integer.parseInt("" + s2.charAt(i)));
+        }
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            int firstnum = 0;
+            int secondnum = 0;
+            if (!stack1.isEmpty()) {
+                firstnum = stack1.pop();
+            }
+            if (!stack2.isEmpty()) {
+                secondnum = stack2.pop();
+            }
+
+            int sum = firstnum + secondnum + carry;
+            if (sum > 9) carry = 1;
+            else carry = 0;
+
+            sb.append("" + sum % 10);
+            if (stack1.isEmpty() && stack2.isEmpty() && carry != 0) {
+                sb.append(carry);
+            }
+
+        }
+        System.out.println(sb.reverse().toString());
+
+    }
+}
+

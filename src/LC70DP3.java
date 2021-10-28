d3911ab2c26c830a6908e8086216fa067ecb07de
micgogi <rahul.gogyani@gmail.com>
2021-10-28 18:48:42 +0530

    LC70DP3

---
+++ src/LC70DP3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 10/28/2021  6:47 PM
+ * Rahul Gogyani
+ */
+public class LC70DP3 {
+    public static void main(String args[]) {
+        System.out.println(climbStairs(30));
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+
+    }
+}
+

246692896513a0e788e62d9ca84707882d577963
micgogi <rahul.gogyani@gmail.com>
2021-11-09 19:26:16 +0530

    LC122

---
+++ src/LC122PractBuyandsell.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 11/9/2021  7:21 PM
+ * Rahul Gogyani
+ */
+public class LC122PractBuyandsell {
+    public static void main(String args[]) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        return max;
+    }
+
+}
+

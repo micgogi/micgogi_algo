d7f09e6138b00b8d51557a251c48308caa9e6501
micgogi <rahul.gogyani@gmail.com>
2022-01-04 12:44:38 +0530

    LC1025

---
+++ src/LC1025.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Micgogi
+ * on 1/4/2022  12:38 PM
+ * Rahul Gogyani
+ */
+public class LC1025 {
+    public static void main(String args[]) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        System.out.println(new LC1025().divisorGame(t));
+    }
+
+    public boolean divisorGame(int n) {
+        if(n%2==0)return true;
+        return false;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in), 32768);
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[][] read2DArray(int n) {
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+        int[][] read2DArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+    }
+
+}
+

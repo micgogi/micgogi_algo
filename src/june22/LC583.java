abaef177d279b7b257d0ad71ab2a522e5baa9a9b
Rahul Gogyani <rahul.gogyani@gmail.com>
2022-06-14 19:29:13 +0530

    Create LC583.java

---
+++ src/june22/LC583.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+         int dp[][] = new int[s1.length() + 1][s2.length() + 1];
+       
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if(i==0 || j==0)continue;
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) ;
+                }
+            }
+        }
+     return s1.length()+s2.length()- 2*dp[s1.length()][s2.length()];
+    }
+}
